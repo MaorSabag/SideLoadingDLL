3fc4067bac82d19f269fa55ec247c8a1266b589a
Maor Sabag <75939197+MaorSabag@users.noreply.github.com>
2023-09-20 19:02:44 +0300

    Update get_exports.py

--- get_exports.py
+++ get_exports.py
@@ -1,17 +1,31 @@
+import sys
 import pefile
-import argparse
 
-parser = argparse.ArgumentParser(description='Target DLL.')
-parser.add_argument('--target', required=True, type=str,help='Target DLL')
+def proxyFunctions(targetDLL):
+    targetDLL = targetDLL.replace("\\", "/") if "\\" in targetDLL else targetDLL
+    
+    # If our dll can be found in the system32 directory let's not make a copy and telling dll where is the original
+    if targetDLL.lower().startswith("c:/windows/system32"):
 
-args = parser.parse_args()
+        pe = pefile.PE(targetDLL)
+        dll = targetDLL.replace("/", "\\\\").split(".dll")[0]
+        d = [pefile.DIRECTORY_ENTRY["IMAGE_DIRECTORY_ENTRY_EXPORT"]]
+        pe.parse_data_directories(directories=d)
+        exports = [(e.ordinal, e.name.decode()) for e in pe.DIRECTORY_ENTRY_EXPORT.symbols if e.name]
+        pragma_list = []
+        
+        for e in exports:
+            pragma_list.append('#pragma comment(linker,"/export:{func}={dll}.{func},@{ord}")'.format(func=e[1], dll=dll, ord=e[0]))
+        
+        return pragma_list
+    
+    
+def main():
+    if len(sys.argv) != 2:
+        print("Usage main.py <dll path>")
+        exit(1)
+    print(proxyFunctions(sys.argv[1]))
+    
 
-target = args.target
-
-dll = pefile.PE(target)
-
-print("EXPORTS")
-
-for export in dll.DIRECTORY_ENTRY_EXPORT.symbols:
-    if export.name:
-        print(f"    {export.name.decode()}=original.{export.name.decode()} @{export.ordinal}")
+if _name_ == "_main_":
+    main()
